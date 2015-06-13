c45d16cdb0c52f1fcab4566dcd8f0f09ceb83532
Dan Sebastian Thrane <dthrane@gmail.com>
2015-06-13 13:14:59 +0200

    Improved login page.

--- grails-app/assets/javascripts/interactivevideos.js
+++ grails-app/assets/javascripts/interactivevideos.js
@@ -5,90 +5,26 @@ var ivids = {};
     var timeline;
     var youtubeVideoId;
     var questions = [];
-    var onNextUpdate = null;
-    var canRemove = true;
 
-    /**
-     * @brief               Places a single input field used in questions.
-     * @details             
-     * 
-     * @param fieldId       Type: String. The ID of the field.
-     * @param evalFunction  Type: (string) => boolean. The function used to evaluate answers.
-     * @param offsetTop     Type: Integer. Y offset relative to top-left corner of player.
-     * @param offsetLeft    Type: Integer. X offset relative to top-left corner of player.
-     */
-    function placeInputField(fieldId, evalFunction, offsetTop, offsetLeft) {
-        $("#wrapper").append(createInputField(fieldId));
-        var field = $("#" + fieldId);
-        field.css({
-            position: "absolute",
-            top: offsetTop + "px", 
-            left: offsetLeft + "px"
-        });
-        field.mathquill("editable");
-    }
-    
-    function initQuestions() {
-        for (var i = timeline.length - 1; i >= 0; i--) {
-            var item = timeline[i];
-            questions = questions.concat(item.questions);
-        }
-    }
-    
-    function buildNavigation() {
-        var nav = $("#videoNavigation");
-        for (var i = 0; i < timeline.length; i++) {
-            var item = timeline[i];
-            var nodeId = "navitem" + String(i);
-            var node = $(createNavItem(item, nodeId));
-    
-            nav.append(node);
-            $("#" + nodeId).click(handleNavigationClick(item));
-    
-            if (item.questions.length > 0) {
-                var list = $("<ul></ul>");
-                nav.append(list);
-                for (var j = 0; j < item.questions.length; j++) {
-                    var question = item.questions[j];
-                    var questionId = "navitem" + String(i) + String(j);
-                    var questionNode = $(createNavItem(question, questionId));
-                    
-                    list.append(questionNode);
-                    $("#" + questionId).click(handleNavigationClick(question));
-                }
-            }
-        }
-    }
-    
-    function handleNavigationClick(item) {
-        return function(e) {
-            var skippedAt = player.currentTime();
-            e.preventDefault();
-            player.currentTime(item.timecode); 
-            player.play();
-            events.emit({
-                kind: "SKIP_TO_CONTENT",
-                video: document.location.href,
-                label: item.title,
-                videoTimeCode: player.currentTime(),
-                skippedAt: skippedAt
-            }, true);
-        };
-    }
-    
     function bootstrap(playerSelector, videoId, tline) {
         timeline = tline;
         youtubeVideoId = videoId;
 
         if (tline !== undefined) {
+            // The time line may be undefined, in this case the video should simply be displayed
             initQuestions();
             buildNavigation();
         }
-    
-        player = Popcorn.smart(playerSelector, "http://www.youtube.com/watch?v=" + youtubeVideoId + "&controls=1");
+
+        var wrapper = Popcorn.HTMLYouTubeVideoElement(playerSelector);
+        wrapper.src = "http://www.youtube.com/watch?v=" + youtubeVideoId + "&controls=0";
+
+        player = Popcorn(wrapper);
         player.on("timeupdate", handleTimeUpdate);
         player.on("seeked", handleSeeked);
-        player.on("play", function() { removeAllQuestions(); }); // Remove all questions when we continue playing
+        player.on("play", function() {
+            removeAllQuestions(); // Remove all questions when we continue playing
+        });
         player.on("pause", function() {
             events.emit({
                 kind: "PAUSE_VIDEO",
@@ -97,14 +33,51 @@ var ivids = {};
             }, true);
         });
         //player.play();
-    
-        $("#checkAnswers").click(function(e) {
+        initEventHandlers();
+    }
+
+    function handleSeeked() {
+        for (var i = questions.length - 1; i >= 0; i--) {
+            questions[i].visible = false;
+            questions[i].shown = false;
+        }
+        hideAllFields();
+        removeAllQuestions();
+        handleTimeUpdate();
+    }
+
+    function handleTimeUpdate() {
+        var timestamp = player.currentTime();
+        for (var i = questions.length - 1; i >= 0; i--) {
+            var q = questions[i];
+            if (q.timecode !== Math.round(timestamp) && q.visible) {
+                removeAllQuestions();
+                q.visible = false;
+            } else if (q.timecode === Math.round(timestamp) && !q.visible && !q.shown) {
+                // Will cause a seek event, which in turn will reset everything
+                player.pause();
+                q.visible = true;
+                q.shown = true;
+                for(var k = 0; k < q.fields.length; k++) {
+                    var field = q.fields[k];
+                    placeInputField(
+                        field.name,
+                        field.answer,
+                        field.topoffset,
+                        field.leftoffset);
+                }
+            }
+        }
+    }
+
+    function initEventHandlers() {
+        $("#checkAnswers").click(function (e) {
             e.preventDefault();
             var question = getVisibleQuestion();
             for (var i = 0; i < question.fields.length; i++) {
                 var field = question.fields[i];
                 var input = $("#" + field.name);
-                var val = parseTex(input.mathquill("latex")); 
+                var val = parseTex(input.mathquill("latex"));
                 input.removeClass("correct error");
                 var correct = validateAnswer(field, val);
                 if (correct) {
@@ -112,7 +85,6 @@ var ivids = {};
                 } else {
                     input.addClass("error");
                 }
-
                 if (val.length > 0) {
                     events.emit({
                         kind: "ANSWER_QUESTION",
@@ -123,17 +95,82 @@ var ivids = {};
             }
             events.flush();
         });
-    
-        $("#frameOverlay").mousemove(function(event) {
-            var parentOffset = $(this).parent().offset(); 
+        $("#frameOverlay").mousemove(function (event) {
+            var parentOffset = $(this).parent().offset();
             var x = parseInt(event.pageX - parentOffset.left);
             var y = parseInt(event.pageY - parentOffset.top);
-    
             var msg = x + ", " + y;
             $("#cursorPosition").text(msg);
         });
     }
-    
+
+    function initQuestions() {
+        for (var i = timeline.length - 1; i >= 0; i--) {
+            var item = timeline[i];
+            questions = questions.concat(item.questions);
+        }
+    }
+
+    function buildNavigation() {
+        var nav = $("#videoNavigation");
+        for (var i = 0; i < timeline.length; i++) {
+            var item = timeline[i];
+            var nodeId = "navitem" + String(i);
+            var node = $(createNavItem(item, nodeId));
+
+            nav.append(node);
+            $("#" + nodeId).click(handleNavigationClick(item));
+
+            if (item.questions.length > 0) {
+                var list = $("<ul></ul>");
+                nav.append(list);
+                for (var j = 0; j < item.questions.length; j++) {
+                    var question = item.questions[j];
+                    var questionId = "navitem" + String(i) + String(j);
+                    var questionNode = $(createNavItem(question, questionId));
+
+                    list.append(questionNode);
+                    $("#" + questionId).click(handleNavigationClick(question));
+                }
+            }
+        }
+    }
+
+    function handleNavigationClick(item) {
+        return function(e) {
+            var skippedAt = player.currentTime();
+            e.preventDefault();
+            player.currentTime(item.timecode);
+            events.emit({
+                kind: "SKIP_TO_CONTENT",
+                video: document.location.href,
+                label: item.title,
+                videoTimeCode: player.currentTime(),
+                skippedAt: skippedAt
+            }, true);
+        };
+    }
+
+    /**
+     * @brief               Places a single input field used in questions.
+     * @details
+     *
+     * @param fieldId       Type: String. The ID of the field.
+     * @param evalFunction  Type: (string) => boolean. The function used to evaluate answers.
+     * @param offsetTop     Type: Integer. Y offset relative to top-left corner of player.
+     * @param offsetLeft    Type: Integer. X offset relative to top-left corner of player.
+     */
+    function placeInputField(fieldId, evalFunction, offsetTop, offsetLeft) {
+        $("#wrapper").append(createInputField(fieldId));
+        var field = $("#" + fieldId);
+        field.css({
+            position: "absolute",
+            top: offsetTop + "px",
+            left: offsetLeft + "px"
+        });
+        field.mathquill("editable");
+    }
+
     function validateAnswer(field, value) {
         var answer = field.answer;
         switch (answer.type) {
@@ -161,7 +198,7 @@ var ivids = {};
                 return answer.validator(value);
         }
     }
-    
+
     function getVisibleQuestion() {
         for (var i = 0; i < timeline.length; i++) {
             var item = timeline[i];
@@ -174,69 +211,18 @@ var ivids = {};
         }
         return null;
     }
-    
+
     function removeAllQuestions() {
         $(".question").remove();
         hideAllFields();
     }
-    
-    function handleTimeUpdate() {
-        console.log("Time update");
-        if (onNextUpdate !== null) {
-            onNextUpdate();
-            onNextUpdate = null;
-            console.log("Skipping");
-            return;
-        }
 
-        var timestamp = player.currentTime();
-        console.log(timestamp);
-        for (var i = questions.length - 1; i >= 0; i--) {
-            var q = questions[i];
-            if (q.timecode !== Math.round(timestamp) && q.visible) {
-                removeAllQuestions();
-                q.visible = false;
-            } else if (q.timecode === Math.round(timestamp) && !q.visible && !q.shown) {
-                // Will cause a seek event, which in turn will reset everything
-                var thisQuestion = q;
-                canRemove = false;
-                onNextUpdate = function() {
-                    player.pause();
-                    thisQuestion.visible = true;
-                    thisQuestion.shown = true;
-                    for(var k = 0; k < q.fields.length; k++) {
-                        var field = q.fields[k];
-                        placeInputField(
-                            field.name,
-                            field.answer,
-                            field.topoffset,
-                            field.leftoffset);
-                    }
-                    canRemove = true;
-                };
-                player.currentTime(q.timecode + 1);
-            }
-        }
-    }
-    
     function hideAllFields() {
-        if (!canRemove) return;
         for (var i = questions.length - 1; i >= 0; i--) {
             questions[i].visible = false;
         }
     }
-    
-    function handleSeeked() {
-        console.log("seek");
-        for (var i = questions.length - 1; i >= 0; i--) {
-            questions[i].visible = false;
-            questions[i].shown = false;
-        }
-        hideAllFields();
-        removeAllQuestions();
-        handleTimeUpdate();
-    }
-    
+
     function createInputField(id) {
         return '<span class="question" id="' + id + '"></span>';
     }
@@ -276,5 +262,6 @@ var ivids = {};
     }
 
     ivids.bootstrap = bootstrap;
+    ivids.player = function() { return player; };
     return ivids;
 })(ivids);
--- grails-app/views/login/auth.gsp
+++ grails-app/views/login/auth.gsp
@@ -1,36 +1,35 @@
+<%@ page import="dk.danthrane.twbs.ButtonStyle" %>
 <html>
 
 <head>
-    <title><g:message code='spring.security.ui.login.title'/></title>
+    <title>Log ind</title>
     <meta name='layout' content="main"/>
 </head>
 
 <body>
 
-<div class="container">
-    <div class="row">
-        <form action='${postUrl}' method='POST' id="loginForm" name="loginForm" autocomplete='off' class="form-signin">
-            <h2 class="form-signin-heading">Please sign in</h2>
-            <label for="inputUsername" class="sr-only"><g:message code='spring.security.ui.login.username'/></label>
-            <input id="inputUsername" class="form-control" placeholder="Username" name="j_username" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="j_password" required>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="${rememberMeParameter}" id="remember_me"> Remember me
-                </label>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <s2ui:linkButton elementId='register' controller='register' messageCode='spring.security.ui.login.register' class="btn btn-link btn-block"/>
-                </div>
-                <div class="col-md-6">
-                    <s2ui:submitButton elementId='loginButton' form='loginForm' messageCode='spring.security.ui.login.login' class="btn btn-primary btn-block"/>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
+<twbs:row>
+    <twbs:form action="${postUrl}" method="POST" id="loginForm" name="loginForm" autocomplete="off" class="form-signin">
+        <twbs:pageHeader>
+            <h1>Log ind</h1>
+        </twbs:pageHeader>
+        <twbs:input name="j_username" labelText="Brugernavn" />
+        <twbs:input name="j_password" type="password" labelText="Kodeord" />
+        <twbs:checkbox name="rememberMeParameter" id="remember_me" labelText="Forbliv logget ind" />
+        <twbs:row>
+            <twbs:column cols="6">
+                <twbs:linkButton elementId="register" controller="register" style="${ButtonStyle.LINK}" block="true">
+                    Glemt kodeord?
+                </twbs:linkButton>
+            </twbs:column>
+            <twbs:column cols="6">
+                <twbs:button type="submit" style="${ButtonStyle.PRIMARY}" block="true">
+                    Log ind
+                </twbs:button>
+            </twbs:column>
+        </twbs:row>
+    </twbs:form>
+</twbs:row>
 
 <script>
     $(document).ready(function() {
