eb2e6b814635af8e5b4a4a640ea74060d418e599
Dan Sebastian Thrane <dthrane@gmail.com>
2015-12-01 09:52:14 +0100

    Initial code structure for the dashboard

--- grails-app/domain/dk/sdu/tekvideo/Teacher.groovy
+++ grails-app/domain/dk/sdu/tekvideo/Teacher.groovy
@@ -14,4 +14,19 @@ class Teacher {
         if (alias != null) return alias
         return user.username
     }
+
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+
+        Teacher teacher = (Teacher) o
+
+        if (id != teacher.id) return false
+
+        return true
+    }
+
+    int hashCode() {
+        return (id != null ? id.hashCode() : 0)
+    }
 }
