cd4f7eb7a3ac5b1345a2ef039b903711ac34c16d
Dan Sebastian Thrane <dthrane@gmail.com>
2016-07-28 13:22:29 +0200

    Removes old debugging code

--- grails-app/domain/dk/sdu/tekvideo/UserRole.groovy
+++ grails-app/domain/dk/sdu/tekvideo/UserRole.groovy
@@ -40,8 +40,6 @@ class UserRole implements Serializable {
 	}
 
 	static UserRole create(User user, Role role, boolean flush = false) {
-		println user
-        println role
 		def instance = new UserRole(user: user, role: role)
 		instance.save(flush: flush, insert: true)
 		instance
